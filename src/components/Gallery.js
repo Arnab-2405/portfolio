a601efe43ed24a67a138f67fd8799fd0f54a8716
Arnab <arnabdastriker@gmail.com>
2024-05-01 01:38:35 +0530

    gallery section font & colour complete

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,16 +5,21 @@ import dataSource from "../datasets/gallery.json";
 function Gallery() {
   return (
     <>
-      <div className={styles.header} id="gallery">
-        Some of the best Moments Captured by me
-      </div>
+      <div className={styles.background}>
+        <div
+          className={[styles.header].join(" ")}
+          id="gallery"
+        >
+          Some of the best Moments Captured by me
+        </div>
 
-      <div className={styles.content}>
-        {dataSource.map((image, index) => (
-          <div key={index} className={styles.box}>
-            <img src={image.imgSrc} alt={image.alt}></img>
-          </div>
-        ))}
+        <div className={[styles.content].join(" ")}>
+          {dataSource.map((image, index) => (
+            <div key={index} className={styles.box}>
+              <img src={image.imgSrc} alt={image.alt}></img>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
