9d216f2cc4665b886f15ee886561b08c438d70db
Arnab <arnabdastriker@gmail.com>
2024-05-10 19:45:35 +0530

    timeline skeletion complete

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,33 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import style from '../stylesheets/Timeline.module.css';
+import React, { useState, useEffect } from "react";
+import style from "../stylesheets/Timeline.module.css";
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "../assets/animate.json";
+import cgi from "../assets/company/cgi.jpg"
 
 function Timeline() {
   const [filledHeight, setFilledHeight] = useState(0);
 
   useEffect(() => {
     function updateFilledHeight() {
-      const newHeight = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+      const newHeight =
+        (window.scrollY / (document.body.scrollHeight - window.innerHeight)) *
+        100;
       setFilledHeight(newHeight);
     }
 
-    // Add scroll event listener
-    window.addEventListener('scroll', updateFilledHeight);
+    window.addEventListener("scroll", updateFilledHeight);
 
-    // Call once initially to set the initial filled height
     updateFilledHeight();
 
-    // Cleanup function
     return () => {
-      window.removeEventListener('scroll', updateFilledHeight);
+      window.removeEventListener("scroll", updateFilledHeight);
     };
-  }, []); // Empty dependency array to run effect only once
+  }, []);
 
   return (
     <div className={style.flexbox}>
       <div className={style.timeline}>
-        <div className={style.filled} style={{ height: `${filledHeight}%` }}></div>
+        <div
+          className={style.filled}
+          style={{ height: `${filledHeight}%` }}
+        ></div>
       </div>
-      <div className={style.rightCard}><div>C2</div><div>VC3</div></div>
+      <div className={style.rightCard}>
+        <div className={style.content}>
+          <>
+            <div className={style.arrow}></div>
+            <div className={style.card}>
+              <img src={cgi} alt="image"></img>
+              <div className={style.text}>
+                <h3>Software Developer</h3>
+                <h4>2023 - Present</h4>
+                <h5>CGI Inc.</h5>
+              </div>
+            </div>
+            <div className={style.pseduo}></div>
+          </>
+        </div>
+        <div className={style.svg}>
+          <Lottie className={style.image} animationData={groovyWalkAnimation} loop={true} />
+          </div>
+      </div>
     </div>
   );
 }
